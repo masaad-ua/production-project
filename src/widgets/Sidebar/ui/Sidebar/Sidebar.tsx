fb914b4589a3bbb5ce14009ac819074a3ff5f563
MaksymOsa <maksym3d@gmail.com>
2026-01-23 08:20:49 +0200

    fix toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
+import {t} from "i18next";
 
 interface SidebarProps {
     className?: string;
@@ -25,7 +26,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                toggle
+                {t("Переключать")}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
