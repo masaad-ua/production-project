e4925a9c3a7df32318cfc43f562dd19de0833edc
MaksymOsa <maksym3d@gmail.com>
2026-03-03 10:35:21 +0200

    #63 add react virtualized

--- src/pages/ArticleDetailsPage/model/slices/articleDetailPageRecommendationsSlice.ts
+++ src/pages/ArticleDetailsPage/model/slices/articleDetailPageRecommendationsSlice.ts
@@ -1,26 +1,19 @@
-import { createEntityAdapter, createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-import { Comment } from 'entities/Comment';
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
-import {
-    fetchCommentsByArticleId,
-} from 'pages/ArticleDetailsPage/model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
-import {
-    ArticleDetailsRecommendationsSchema,
-} from 'pages/ArticleDetailsPage/model/types/ArticleDetailsRecommendationsSchema';
 import { Article } from 'entities/Article';
-import { ArticleDetailsCommentsSchema } from '../types/ArticleDetailsCommentsSchema';
+import { fetchArticleRecommendations } from '../services/fetchArticleRecommendations/fetchArticleRecommendations';
+import { ArticleDetailsRecommendationsSchema } from '../types/ArticleDetailsRecommendationsSchema';
 
 const recommendationsAdapter = createEntityAdapter<Article>({
     selectId: (article) => article.id,
 });
 
-export const getArticleComments = recommendationsAdapter.getSelectors<StateSchema>(
-    (state) => state.articleDetailsComments || recommendationsAdapter.getInitialState(),
+export const getArticleRecommendations = recommendationsAdapter.getSelectors<StateSchema>(
+    (state) => state.articleDetailsPage?.recommendations || recommendationsAdapter.getInitialState(),
 );
 
-const articleDetailPageRecommendationsSlice = createSlice({
-    name: 'articleDetailsCommentsSlice',
+const articleDetailsPageRecommendationsSlice = createSlice({
+    name: 'articleDetailsPageRecommendationsSlice',
     initialState: recommendationsAdapter.getInitialState<ArticleDetailsRecommendationsSchema>({
         isLoading: false,
         error: undefined,
@@ -30,22 +23,24 @@ const articleDetailPageRecommendationsSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(fetchCommentsByArticleId.pending, (state) => {
+            .addCase(fetchArticleRecommendations.pending, (state) => {
                 state.error = undefined;
                 state.isLoading = true;
             })
-            .addCase(fetchCommentsByArticleId.fulfilled, (
+            .addCase(fetchArticleRecommendations.fulfilled, (
                 state,
-                action: PayloadAction<Comment[]>,
+                action,
             ) => {
                 state.isLoading = false;
                 recommendationsAdapter.setAll(state, action.payload);
             })
-            .addCase(fetchCommentsByArticleId.rejected, (state, action) => {
+            .addCase(fetchArticleRecommendations.rejected, (state, action) => {
                 state.isLoading = false;
                 state.error = action.payload;
             });
     },
 });
 
-export const { reducer: articleDetailPageRecommendationsReducer } = articleDetailPageRecommendationsSlice;
+export const {
+    reducer: articleDetailsPageRecommendationsReducer,
+} = articleDetailsPageRecommendationsSlice;
