214a33f408fe6de7648455b0a0bb60a51522097a
MaksymOsa <maksym3d@gmail.com>
2026-03-11 15:23:20 +0200

    #79 add popover, add notifications

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,6 +52,7 @@ module.exports = {
                     'to',
                     'justify',
                     'align',
+                    'border',
                     'direction',
                     'gap',
                     'role'
