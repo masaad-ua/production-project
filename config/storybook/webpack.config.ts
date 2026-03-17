64512e88cac30be71a64e9bc1e857532231a9d5c
MaksymOsa <maksym3d@gmail.com>
2026-03-17 09:38:46 +0200

    #95 remove unused imports

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -16,8 +16,8 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config!.resolve!.extensions!.push('.ts', '.tsx');
     config!.resolve!.alias = {
         ...config!.resolve!.alias,
-        "@": paths.src
-    }
+        '@': paths.src,
+    };
 
     // eslint-disable-next-line no-param-reassign
     // @ts-ignore
