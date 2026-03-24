c912bd627f967250f4493c1435d081631d6d50e4
Maksym Osypchuk <maksym3d@gmail.com>
2026-03-24 10:08:54 +0200

    #130 Add toggle features for component

--- src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ThunkConfig } from 'app/providers/StoreProvider';
 import { Profile } from '../../types/profile';
+import { ThunkConfig } from '@/app/providers/StoreProvider';
 
 export const fetchProfileData = createAsyncThunk<
     Profile,
