192cbf7939b9c1d735eb8154e7de20ddfc7d4700
Maksym Osypchuk <maksym3d@gmail.com>
2026-03-26 14:00:19 +0200

    #152 fix storybook, add feature flags decorator

--- src/app/lib/useAppToolbar.tsx
+++ src/app/lib/useAppToolbar.tsx
@@ -5,7 +5,6 @@ import { useRouteChange } from '@/shared/lib/router/useRouteChange';
 
 export function useAppToolbar() {
     const appRoute = useRouteChange();
-    console.log(appRoute);
 
     const toolbarByAppRoute: OptionalRecord<AppRoutes, ReactElement> = {
         [AppRoutes.ARTICLES]: <ScrollToolbar />,
